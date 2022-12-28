0b775abd191c1c7bcdba5941bacf94ddab27c2b0
Benoit Soupart <Benoit160@live.be>
2022-12-28 14:53:50 +0100

    Moved extensions to separate  file, aded package  info

--- BlazorDB/BlazorDBContext.cs
+++ BlazorDB/BlazorDBContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
@@ -54,39 +53,5 @@ public abstract class BlazorDBContext : DbContext
     {
         await using IJSObjectReference jsModule = await runtime.InvokeAsync<IJSObjectReference>("import", "./_content/Blazor.DB/Cache.js");
         await jsModule.InvokeVoidAsync("DeleteDBFromCache");
-    }
-}
-
-public static class Extensions
-{
-    /// <summary>
-    /// Initialize BlazorDBContext, and restore the database from cache if applicable
-    /// </summary>
-    /// <typeparam name="TContext">Your implementation of BlazorDBContext</typeparam>
-    /// <param name="serviceProvider"></param>
-    /// <returns></returns>
-    public static async Task<IServiceProvider> ConfigureBlazorDBAsync<TContext>(this IServiceProvider serviceProvider)
-        where TContext : BlazorDBContext
-    {
-        BlazorDBContext.ConfigureInterropRuntime(serviceProvider.GetRequiredService<IJSRuntime>());
-
-        bool restoredSuccessfully = await BlazorDBContext.RestoreFromCacheAsync();
-
-        if (!restoredSuccessfully)
-        {
-            // check if BlazorDBContext subtype has default parameterless constructor
-            if (typeof(TContext).GetConstructors().Any(x => x.GetParameters().Length == 0))
-            {
-                // creates an instance of the BlazorDBContext subtype, to initialize it's database
-                TContext dbContextInstance = (Activator.CreateInstance(typeof(TContext)) as TContext)!;
-
-                // makes sure the database is correctly created
-                await dbContextInstance.Database.EnsureCreatedAsync();
-            }
-            else
-                throw new Exception($"{typeof(TContext).Name} must have a public parameterless constructor");
-        }
-
-        return serviceProvider;
     }
 }
---
+++ BlazorDB/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+
+namespace BlazorDB;
+
+public static class Extensions
+{
+    /// <summary>
+    /// Initialize BlazorDBContext, and restore the database from cache if applicable
+    /// </summary>
+    /// <typeparam name="TContext">Your implementation of BlazorDBContext</typeparam>
+    /// <param name="serviceProvider"></param>
+    /// <returns></returns>
+    public static async Task<IServiceProvider> ConfigureBlazorDBAsync<TContext>(this IServiceProvider serviceProvider)
+        where TContext : BlazorDBContext
+    {
+        BlazorDBContext.ConfigureInterropRuntime(serviceProvider.GetRequiredService<IJSRuntime>());
+
+        bool restoredSuccessfully = await BlazorDBContext.RestoreFromCacheAsync();
+
+        if (!restoredSuccessfully)
+        {
+            // check if BlazorDBContext subtype has default parameterless constructor
+            if (typeof(TContext).GetConstructors().Any(x => x.GetParameters().Length == 0))
+            {
+                // creates an instance of the BlazorDBContext subtype, to initialize it's database
+                TContext dbContextInstance = (Activator.CreateInstance(typeof(TContext)) as TContext)!;
+
+                // makes sure the database is correctly created
+                await dbContextInstance.Database.EnsureCreatedAsync();
+            }
+            else
+                throw new Exception($"{typeof(TContext).Name} must have a public parameterless constructor");
+        }
+
+        return serviceProvider;
+    }
+}
